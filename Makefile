2b849b7b3c6358d7c88bde0b289c4a628478d127
Abraham Ajibade <97408939+jibbs1703@users.noreply.github.com>
2025-03-22 13:44:37 -0400

    ETL: Extract Data (#2)
    - Completed scripts to extract tickit sales data from the on-premises NoSQL database
    - Completed scripts to upload data to the staging area in AWS S3
    - Added tests for the extraction module of ETL

---
+++ Makefile
@@ -0,0 +1,17 @@
+# Default commit message
+COMMIT_MSG ?= "refactored tests for the data extraction module and added Makefile"
+
+# Target to add changes to staging
+add:
+	   git add .
+
+# Target to commit with a message
+commit: add
+	    git commit -m $(COMMIT_MSG)
+
+# Target to push to the default branch
+push: commit
+	     git push
+
+# Run all targets
+all: push
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,27 @@
 """Fixtures for module tests."""
 
+from unittest.mock import MagicMock, patch
+
 import pytest
+from pymongo.collection import Collection
+from pymongo.database import Database
 
 
 @pytest.fixture
 def mock_connection(mocker):
     """Fixture to mock MongoClient."""
     return mocker.patch("src.extract.extract.MongoClient")
+
+
+@pytest.fixture
+def mock_logger():
+    with patch("src.extract.extract.logger") as mock:
+        yield mock
+
+
+@pytest.fixture
+def mock_db():
+    mock = MagicMock(spec=Database)
+    mock_collection = MagicMock(spec=Collection)
+    mock.__getitem__.return_value = mock_collection
+    return mock, mock_collection
